a087646d55d479c18c09e5bf282904c68100313e
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2022-02-06 19:23:33 +0900

    Create link to my answers

--- update_readme.sh
+++ update_readme.sh
@@ -10,3 +10,11 @@ ls $BASEDIR/problem/*.md \
 | xargs -I@ basename @ .md \
 | sed 's/^0*//' \
 | xargs -I@ sed -i "/問題 @:/s/\[ \]/\[x\]/" $BASEDIR/README.md
+
+tempfile=$(mktemp)
+awk '
+  /\[x\] 練習/ { printf("- [x] [%s](practice/%s.md)\n", substr($0, 7), substr($4, 1, length($4) - 1)); next }
+  /\[x\] 問題/ { printf("- [x] [%s](problem/%03d.md)\n", substr($0, 7), $4); next }
+  { print $0 }
+' $BASEDIR/README.md >>$tempfile \
+&& mv $tempfile $BASEDIR/README.md
